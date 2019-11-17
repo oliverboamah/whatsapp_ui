13cec490f8fb98b719799bd1f0b60a2b7368dfd7
codingoliver <oliverboamah@yahoo.com>
2019-11-17 07:22:48 +0000

    Create Camera Tab, Status Tab, Call Tab

---
+++ lib/ui/tabs/call_tab.dart
@@ -0,0 +1,16 @@
+// flutter imports
+import 'package:flutter/widgets.dart';
+
+class CallTab extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CallTabState();
+}
+
+class _CallTabState extends State<CallTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Call Tab'),
+    );
+  }
+}
