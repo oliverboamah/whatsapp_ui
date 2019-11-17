11d469375aae3122592ec6c7cc5cd7cd0650dffe
codingoliver <oliverboamah@yahoo.com>
2019-11-17 08:45:30 +0000

    Make Tabs swipeable

--- lib/ui/layouts/app_bar.dart
+++ lib/ui/layouts/app_bar.dart
@@ -1,25 +1,35 @@
+// flutter imports
 import 'package:flutter/material.dart';
+
+// my app imports
 import 'package:whatsapp_ui/env.dart';
 
-class HomePageAppBar {
-  AppBar getAppBar(BuildContext context) {
-    return AppBar(
-        title: Text(APP_NAME),
-        elevation: 0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () => {},
-          ),
-          IconButton(icon: Icon(Icons.more_vert), onPressed: () => {})
-        ],
-        bottom: TabBar(
-          tabs: [
-            Tab(icon: Icon(Icons.camera_alt)),
-            Tab(text: 'CHATS',),
-            Tab(text: 'STATUS'),
-            Tab(text: 'CALLS',),
-          ],
-        ));
-  }
+class HomePageAppBar extends AppBar {
+  final TabController tabController;
+  final Function onTab;
+  HomePageAppBar({@required this.tabController, @required this.onTab})
+      : super(
+            title: Text(APP_NAME),
+            elevation: 0,
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () => {},
+              ),
+              IconButton(icon: Icon(Icons.more_vert), onPressed: () => {})
+            ],
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.camera_alt)),
+                Tab(
+                  text: 'CHATS',
+                ),
+                Tab(text: 'STATUS'),
+                Tab(
+                  text: 'CALLS',
+                ),
+              ],
+              onTap: (index) => onTab(index),
+              controller: tabController,
+            ));
 }
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,27 +1,55 @@
+// flutter imports
 import 'package:flutter/material.dart';
+
+// my app imports
 import 'package:whatsapp_ui/ui/layouts/app_bar.dart';
+import 'package:whatsapp_ui/ui/tabs/camera_tab.dart';
 import 'package:whatsapp_ui/ui/tabs/chat_tab.dart';
+import 'package:whatsapp_ui/ui/tabs/status_tab.dart';
+import 'package:whatsapp_ui/ui/tabs/call_tab.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  // selected tab index
-  int _selectedTabIndex = 0;
-
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   // tabs
-  List<Widget> _tabs = [ChatTab()];
+  List<Widget> _tabs = [CameraTab(), ChatTab(), StatusTab(), CallTab()];
+
+  // page controller
+  final _pageController = PageController(initialPage: 0);
+
+  // tab controller
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    this._tabController = TabController(vsync: this, length: 4);
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 4,
       child: Scaffold(
-        appBar: HomePageAppBar().getAppBar(context),
-        body: this._tabs[this._selectedTabIndex],
+        appBar: HomePageAppBar(
+          tabController: this._tabController,
+          onTab: (index) {
+            this.setState(() {
+              this._pageController.jumpToPage(index);
+            });
+          },
+        ),
+        body: PageView(
+          controller: this._pageController,
+          children: this._tabs,
+          onPageChanged: (index) => this._tabController.animateTo(index),
+        ),
       ),
+      initialIndex: 0,
     );
   }
 }
