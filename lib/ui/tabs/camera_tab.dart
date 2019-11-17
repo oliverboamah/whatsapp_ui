7a83cbe0fd5a7c53a7f482fb616446e199f82123
codingoliver <oliverboamah@yahoo.com>
2019-11-17 07:24:44 +0000

    Update Camera Tab, Status Tab

---
+++ lib/ui/tabs/camera_tab.dart
@@ -0,0 +1,16 @@
+// flutter imports
+import 'package:flutter/widgets.dart';
+
+class CameraTab extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CameraTabState();
+}
+
+class _CameraTabState extends State<CameraTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Camera Tab'),
+    );
+  }
+}
---
+++ lib/ui/tabs/status_tab.dart
@@ -0,0 +1,16 @@
+// flutter imports
+import 'package:flutter/widgets.dart';
+
+class StatusTab extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _StatusTabState();
+}
+
+class _StatusTabState extends State<StatusTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Status Tab'),
+    );
+  }
+}
