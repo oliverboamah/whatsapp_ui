ed858fc25a647156578e43019a34f2dce833f11e
codingoliver <oliverboamah@yahoo.com>
2019-11-17 09:52:46 +0000

    Add FABs to Status Tab

--- lib/ui/tabs/status_tab.dart
+++ lib/ui/tabs/status_tab.dart
@@ -1,5 +1,8 @@
 // flutter imports
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:whatsapp_ui/ui/values/colors.dart';
 
 class StatusTab extends StatefulWidget {
   @override
@@ -9,8 +12,30 @@ class StatusTab extends StatefulWidget {
 class _StatusTabState extends State<StatusTab> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Status Tab'),
+    return Scaffold(
+      body: Container(),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: FloatingActionButton(
+              child: Icon(
+                Icons.edit,
+                color: PRIMARY_COLOR,
+              ),
+              mini: true,
+              backgroundColor: Colors.grey.shade200,
+              onPressed: () => {},
+            ),
+          ),
+          FloatingActionButton(
+            child: Icon(CupertinoIcons.photo_camera_solid),
+            onPressed: () => {},
+            backgroundColor: ACCENT_COLOR,
+          )
+        ],
+      ),
     );
   }
 }
